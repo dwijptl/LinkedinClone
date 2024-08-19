667662e28dd541012394569bd477c52dceef6ef6
dwijp <dpatel0536@conestogac.on.ca>
2024-08-19 13:34:14 -0400

    Project setup

---
+++ linkedinclone/src/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src='https://www.svgrepo.com/show/75820/linkedin.svg' alt=""></img>
+
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text"></input>
+        </div>
+      </div>
+
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default Header;
