8199b4326e6283179eeef417c06f020ac8ae86c4
dwijp <dpatel0536@conestogac.on.ca>
2024-08-19 18:07:02 -0400

    Project setup and created header

--- linkedinclone/src/Header.js
+++ linkedinclone/src/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import "./Header.css";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 function Header() {
   return (
@@ -14,7 +20,17 @@ function Header() {
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://th.bing.com/th/id/R.84e88962c3e88f9034a336b1f1882b52?rik=CllGHO5YUgL6Uw&pid=ImgRaw&r=0"
+          title="Me"
+        />
+      </div>
     </div>
   );
 }
